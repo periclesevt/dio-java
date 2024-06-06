ef79e6a8d9c535669fe7cb1c8641c6790077c233
periclesevt <pericles.vasconcelos@pathfind.com.br>
2024-06-06 01:20:56 -0300

    Criação e lógica exercício ContaTerminal

---
+++ ContaTerminal.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    static int numeroConta;
+    static String numeroAgencia;
+    static String nomeCLiente;
+    static float saldoConta;
+
+    public static void CriarConta(){
+        Scanner scanner  = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta! :");
+        numeroConta = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência! :");
+        numeroAgencia = scanner.next();
+
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente! :");
+        nomeCLiente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da Conta! :");
+        saldoConta = scanner.nextFloat();
+
+        scanner.close();
+
+    }
+
+    public static void MensagemSistema(){
+        System.out.println("Olá " + nomeCLiente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta +
+                " e seu saldo " + saldoConta + " já está disponível para saque. ");
+    }
+
+
+    public static void main(String[] args) {
+        CriarConta();
+        MensagemSistema();
+    }
+}
