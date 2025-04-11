b699361aecdc1e5ccb496ec30d230f39ca1e0203
periclesevt <periclesevt@gmail.com>
2025-04-11 20:20:12 -0300

    add Main para testes

--- src/AplicativosMensagem/ComputadorPedrinho.java
+++ src/AplicativosMensagem/ComputadorPedrinho.java
@@ -4,24 +4,24 @@ package AplicativosMensagem;
  * Simulação do uso da classe MSNMessenger
  */
 public class ComputadorPedrinho {
-    public static void main(String[] args) {
-
-        ServicoMensagemInstantanea smi = null;
-
-        //Escolher
-        String appEscolhido = "fb";
-
-        if(appEscolhido.equals("msn")){
-            smi = new MSNMessenger();
-        } else if (appEscolhido.equals("fb")) {
-            smi = new FacebookMessenger();
-        } else if (appEscolhido.equals("tlg")) {
-            smi = new Telegram();
-        }
-
-        smi.enviarMensagem();
-        smi.receberMensagem();
-        smi.salvarHistoricoMensagem();
-
-    }
+//    public static void main(String[] args) {
+//
+//        ServicoMensagemInstantanea smi = null;
+//
+//        //Escolher
+//        String appEscolhido = "fb";
+//
+//        if(appEscolhido.equals("msn")){
+//            smi = new MSNMessenger();
+//        } else if (appEscolhido.equals("fb")) {
+//            smi = new FacebookMessenger();
+//        } else if (appEscolhido.equals("tlg")) {
+//            smi = new Telegram();
+//        }
+//
+//        smi.enviarMensagem();
+//        smi.receberMensagem();
+//        smi.salvarHistoricoMensagem();
+//
+//    }
 }
---
+++ src/ContaBancaria/Main.java
@@ -0,0 +1,48 @@
+package ContaBancaria;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ContaBancaria conta = new ContaBancaria(300);
+
+        exibirDadosConta(conta);
+
+
+    }
+
+    private static void exibirDadosConta(ContaBancaria conta){
+        System.out.println("Saldo: R$" + conta.consultarSaldo());
+        System.out.println("Limite Cheque Especial: R$" + conta.consultarChequeEspecial());
+        System.out.println("Usando Cheque Especial?" + (conta.estaUsandoChequeEspecial() ? "Sim" : "Nao"));
+    }
+
+
+    private static void realizarSaque(ContaBancaria conta, double valor){
+        try{
+            conta.sacar(valor);
+                    System.out.println("Saque de R$" + valor + "realizado");
+        } catch (IllegalArgumentException e){
+            System.out.println("Erro no saque: " + e.getMessage());
+        }
+    }
+
+    private static void realizarDeposito(ContaBancaria conta, double valor){
+        try{
+            conta.depositar(valor);
+                    System.out.println("Deposito de R$" + valor + "realizado");
+        } catch (IllegalArgumentException e){
+            System.out.println("Erro no deposito: " + e.getMessage());
+        }
+    }
+
+    private static void pagarBoleto(ContaBancaria conta, double valor){
+        try{
+            conta.pagarBoleto(valor);
+            System.out.println("Boleto de R$" + valor + "pago");
+        } catch (IllegalArgumentException e){
+            System.out.println("Erro no pagamento: " + e.getMessage());
+        }
+    }
+}
+
+
